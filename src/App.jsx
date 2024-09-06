24eac856239f974a94df24f99e436ffcca2a5672
sanathsb98 <itssanathsb@gmail.com>
2024-09-06 10:23:42 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SetNewPassword from './pages/SetNewPassword';
 import Home from './pages/Home';
 import ProtectedRoutes from './routes/ProtectedRoutes';
+import Navbar from './components/Navbar';
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
-        <Routes>
+      
+     <Routes>
           <Route path='/signup' element={<SignUp />} />
           <Route path='/' element={<SignIn />} />
           <Route path='/resetpassword' element={<ResetPass />} />
           <Route path='/users/verifyResetToken' element={<SetNewPassword />} />
-          <Route path='/home' element={<ProtectedRoutes element={Home} />} /> {/* Use ProtectedRoute here */}
+          <Route path='/home' element={<ProtectedRoutes element={Home} />} /> 
         </Routes>
       </BrowserRouter>
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from 'react';
+import '../pages/Home.css';
+import Navbar from '../components/Navbar';
 
 const Home = () => {
   return (
-    <div>Homes</div>
+  
+    <div className='home-page'>
+
+   {/* total navbar height is 100px and 60px goes to the navbar offer height */}
+      <div className='navbar-section'>
+        <Navbar />
+      </div>
+      
+      {/* margin top 100px */}
+      <div className='home-content'>
+
+        <div className='home-slider-space'>
+          slider
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
