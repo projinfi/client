757c983130b36ba265fdc5c0200b8863f2380ba0
sanathsb98 <itssanathsb@gmail.com>
2024-09-26 15:02:53 +0530

    commit

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import '../components/CartItem.css';
-import cartImg from '../assets/products/product1.png'
+import cartImg from '../assets/products/product1.png';
+import { useDispatch } from 'react-redux';
+import { removeReduxCart } from '../slices/cartSlice';
 
 const CartItem = ({data}) => {
+
+    const dispatch = useDispatch()
+
+    const removeFromCart = (data) => {
+     dispatch(removeReduxCart({id:data.id}))
+    }
+
     return (
         <div className='cart-item-box'>
             <div className='cart-item-left'>
@@ -10,7 +19,7 @@ const CartItem = ({data}) => {
             </div>
             <div className='cart-item-right'>
                 <div className='cart-item-name'>{data.product_name}</div>
-                <div className='cart-item-cancel'>X Remove</div>
+                <div onClick={()=>{removeFromCart(data)}} className='cart-item-cancel'>X Remove</div>
             </div>
         </div>
     )
--- src/slices/cartSlice.js
+++ src/slices/cartSlice.js
@@ -10,7 +10,7 @@ export const cartSlice = createSlice({
 
             const { id, product_name, product_des, product_price, stock_quantity, product_image, order_quantity } = action.payload;
 
-            console.log( id, product_name, product_des, product_price, stock_quantity, order_quantity)
+            console.log(id, product_name, product_des, product_price, stock_quantity, order_quantity)
 
             const existingProduct = state.find((item) => item.id === id)
 
@@ -27,10 +27,14 @@ export const cartSlice = createSlice({
                     order_quantity
                 })
             }
-
+        },
+        removeReduxCart(state,action){
+        const {id} = action.payload;
+        return state.filter((item) => item.id !== id)
         }
     }
 })
 
-export const { addToReduxCart } = cartSlice.actions;
+
+export const { addToReduxCart, removeReduxCart} = cartSlice.actions;
 export default cartSlice.reducer;
