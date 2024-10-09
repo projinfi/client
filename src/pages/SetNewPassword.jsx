ae237708eb3eb8735da8164e0f217fddb1f2a7fb
sanathsb98 <itssanathsb@gmail.com>
2024-10-09 15:11:50 +0530

    commit

--- src/pages/SetNewPassword.jsx
+++ src/pages/SetNewPassword.jsx
@@ -71,7 +71,7 @@ const SetNewPassword = () => {
             });
           
             navigate('/');
-            console.log("rest pass data",data[0].response.usertoken)
+            console.log("rest pass data",data)
             console.log("rrrr")
         } catch (err) {
             console.log('Error resetting password', err);
